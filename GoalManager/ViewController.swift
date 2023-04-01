3a6eed03cb3c1382de49bae7fa61a37001532401
Amin Saedi <amin.di.sam@gmail.com>
2023-04-01 06:39:56 -0400

    Initial Commit

---
+++ GoalManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoalManager
+//
+//  Created by Amin Saedi on 2023-04-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
